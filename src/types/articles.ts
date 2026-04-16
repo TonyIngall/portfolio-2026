1e87fe8715621de0728b9bab05956a1db998f7fa
TonyIngall <tonyingall.business@gmail.com>
2026-04-16 11:36:15 +0100

    create articles type to use across site

---
+++ src/types/articles.ts
@@ -0,0 +1,64 @@
+export interface Colour {
+    id: number;
+    documentId: string;
+    name: string;
+    hex: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+}
+
+export interface Category {
+    id: number;
+    documentId: string;
+    name: string;
+    slug: string;
+    description: string | null;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    colour: Colour;
+}
+
+export interface Series {
+    id: number;
+    documentId: string;
+    title: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    locale: string;
+    colour: Colour;
+}
+
+export interface Article {
+    id: number;
+    documentId: string;
+    title: string;
+    description: string | null;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    categories: Category[];
+    series: Series | null;
+}
+
+interface Pagination {
+    page: number;
+    pageSize: number;
+    pageCount: number;
+    total: number;
+}
+
+interface Meta {
+    pagination: Pagination;
+}
+
+export interface StrapiCollectionResponse<T> {
+    data: T[];
+    meta: Meta;
+}
+
+export type ArticlesResponse = StrapiCollectionResponse<Article>;
